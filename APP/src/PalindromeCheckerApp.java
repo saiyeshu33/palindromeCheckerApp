0429904f2f5ae4dd7bacb518d3dabd642935b9a8
yeshwanth reddy <saiyeshu33@gmail.com>
2026-03-16 20:50:06 +0530

    Case-Insensitive & Space-Ignored Palindrome

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp{
 
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
+    public static boolean isPalindrome(String str) {
+        // Normalize string: remove spaces and convert to lowercase
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        boolean result = isPalindrome(input, 0, input.length() - 1);
-
-        if (result) {
-            System.out.println("Palindrome");
+        if (isPalindrome(input)) {
+            System.out.println("It is a palindrome (ignoring case and spaces).");
         } else {
-            System.out.println("Not a Palindrome");
+            System.out.println("It is not a palindrome.");
         }
 
         sc.close();
